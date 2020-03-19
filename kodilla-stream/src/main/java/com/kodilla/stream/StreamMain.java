cf6c776277f143ec74a1a252557a5d1483317676
lisowski90 <lisowski1990@gmail.com>
2020-03-19 16:57:54 +0100

    7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -10,5 +11,8 @@ public class StreamMain {
         poemBeautifier.beautify("Ambroży", text -> "ABC " + text + " ABC");
         poemBeautifier.beautify("Ambroży", text -> text.replaceFirst("A", "B"));
         poemBeautifier.beautify("Ambroży", text -> text.substring(3));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }

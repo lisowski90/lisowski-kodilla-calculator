b961b83cf49e7029fd68c35b4bdbcc0cdd867b5c
lisowski90 <lisowski1990@gmail.com>
2020-03-24 12:58:38 +0100

    8.3

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -8,7 +8,7 @@ import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class FileReader {
-    public void readFile() {
+    public void readFile() throws FileReaderException{
 
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("files/names.txt").getFile());
@@ -19,7 +19,7 @@ public class FileReader {
 
         } catch(IOException e) {
 
-            System.out.println("Oh no! Something went wrong!" + e);
+            throw new FileReaderException();
         } finally {
             System.out.println("I am gonna be here ... always!");
         }

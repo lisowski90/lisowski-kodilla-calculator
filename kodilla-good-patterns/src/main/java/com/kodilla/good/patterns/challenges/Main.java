6e79cd7c1313e6b6ceeb1985f11380035c767b3f
lisowski90 <lisowski1990@gmail.com>
2020-04-03 14:33:50 +0200

    13.1

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Main.java
@@ -0,0 +1,12 @@
+package com.kodilla.good.patterns.challenges;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+        String result = movieStore.getFilms();
+
+        System.out.println(result);
+    }
+}
